148f0ab5e6f9603eba1878bdda2b5695737f0e60
John Oerter <joerter@gmail.com>
2016-02-11 13:28:32 -0600

    Add jsxNotEquals, jsxIncludes, and jsxNotIncludes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import jsxEquals from 'tape-jsx-equals';
+import jsxNotEquals from 'tape-jsx-notequals';
+import jsxIncludes from 'tape-jsx-includes';
+import jsxNotIncludes from 'tape-jsx-notincludes';
 
 export default {
-    jsxEquals
+    jsxEquals,
+    jsxNotEquals,
+    jsxIncludes,
+    jsxNotIncludes
 };
